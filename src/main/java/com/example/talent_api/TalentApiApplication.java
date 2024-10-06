b125d3134c351d62bb4e94393cbc7dfb2f8ae6b2
avuzikov <avuzikov@gmail.com>
2024-10-06 15:54:12 +0200

    configure local credentials

--- src/main/java/com/example/talent_api/TalentApiApplication.java
+++ src/main/java/com/example/talent_api/TalentApiApplication.java
@@ -2,11 +2,9 @@ package com.example.talent_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class TalentApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TalentApiApplication.class, args);
 		System.out.println("Hello world!");
